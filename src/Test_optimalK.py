0820721dee1c22650cba71e733ed8dfbd14d74e4
Rita Liu <wliu1@macalester.edu>
2023-02-27 15:49:44 -0500

    update codes with comments
    also included text files showing results

--- src/Test_optimalK.py
+++ src/Test_optimalK.py
@@ -4,19 +4,15 @@ import os
 
 class Test_OptimalK(unittest.TestCase):
     
-    # def test_autocorrelation(self):
-    #     cor_obj = TAQAutocorrelation(['20070919','20070920'],'IBM')
-    #     optimal_f = cor_obj.autocorrelation(['20S','30S','1T','3T','5T','10T','20T'],lags=1)
-
-    #     cor_obj2 = TAQAutocorrelation(['20070919','20070920'],'MSFT')
-    #     optimal_f2 = cor_obj2.autocorrelation(['20S','30S','1T','3T','5T','10T','20T'],lags=1)
-
-    #     self.assertEqual(optimal_f,'30S')
-
+    
     def test_get_all_optimal_k(self):
-        all = OptimalKAll(['IBM','MSFT'],None,None)
+        all = OptimalKAll(['JBL','TMO'],'20070621','20070622')
         all.get_all_optimal_k()
-        self.assertTrue('optimalK.txt' in os.listdir(os.getcwd()))
+        self.assertTrue('optimalK.txt' in os.listdir(os.getcwd()+'/record'))
+        with open(os.getcwd()+'/record/optimalK.txt',mode = 'r+') as f:
+            lastLine = str(f.readlines()[-1]).split(',')
+            f.close()
+        self.assertTrue(lastLine[0],'TMO')
 
 if __name__ == "__main__":
     unittest.main()
--- src/main.py
+++ src/main.py
@@ -12,18 +12,25 @@ from TAQAutocorrelation import AutoCorrAll
 
 # all = OptimalKAll(BaseUtils.snp_tickers,None,None)
 # all.get_all_optimal_k()
+fm1 = FileManager(MyDirectories.getAdjDir())
+fm2 = FileManager(MyDirectories.getCleanDir())
+tickers_dirty = BaseUtils.snp_tickers   
+tickers_corr = fm2.getTradeTickers('20070622')
+tickers_corr.remove('JBL')
+tickers_corr.remove('TMO')
 
+# for t in fm2.getTradeTickers('20070622'):
+#     tickers_dirty.remove(t)
 # obj = TAQCleanTrades(k = 20, tau = 0.0005)
-# obj.cleanAllTrades(tickers=['JBL','TMO'])
-# fm1 = FileManager(MyDirectories.getAdjDir())
-# fm2 = FileManager(MyDirectories.getCleanDir())
+# obj.cleanAllTrades(tickers = tickers_dirty)
+
 
 # reader_before = fm1.getTradesFile( '20070621', 'TMO' )
 # reader_after = fm2.getTradesFile('20070621', 'TMO')
 
 # print(reader_after.getN()/reader_before.getN())
 
-all = AutoCorrAll(['JBL','TMO'],None,None)
+all = AutoCorrAll(tickers_corr,None,None)
 all.get_all_optimal_freq()
 
 
