afe8b8f0c2d4db17732cafaf0081aa273f450978
lakshaygarg <lakshaygarg@users.noreply.github.com>
2023-02-27 00:06:06 -0500

    updated tests need to fix tests for cleaning

--- src/TAQCleaner.py
+++ src/TAQCleaner.py
@@ -5,123 +5,73 @@ from collections import deque
 from statistics import mean, stdev
 import os
 import BaseUtils
+import numpy as np
 
 class TAQCleanTrades():
     """Self suffiecent class for cleaning data
     """
-    
     def __init__(self, k = 5, tau = 0.0005):
        # get adjusted directory
        self._fm = FileManager( MyDirectories.getAdjDir())
        self._k = k
        self._tau = tau
-       
-       #deque for tracking window of size k
-       self._price = deque([],maxlen=self._k)
-
-    # updates parameters on update
-    def setParams(self):
-        self._mean = mean(self._price)
-        self._std = stdev(self._price) 
-        
-    # outlier check rule
-    def checkNotOutlier(self,price):
-        return abs(price - self._mean) < 2*self._std + self._tau*self._mean
 
     # cleans all trade data
     def cleanAllTrades(self, dates = None, tickers = None):
         
         if not dates : dates = BaseUtils.default_dates
         if not tickers : tickers = BaseUtils.snp_tickers
-       
+        
         # get dates
         Dates = self._fm.getTradeDates(dates[0], dates[1])
         for ticker in tickers:
             
             for date in Dates:
                 
-                # get trades file
-                prices = []
-                shares = []
-                ts = []
+                # getting trade file
                 try:
                     tradeData = self._fm.getTradesFile( date, ticker )
                 except:
                     print(ticker, date, "FILE NOT FOUND")
                     continue
+
+                # setting local dataframe for the records    
+                N = tradeData.getN()
+                data = pd.DataFrame({
+                'time':pd.to_numeric(tradeData._ts),
+                'Price':pd.to_numeric(tradeData._p),
+                'Size':pd.to_numeric(tradeData._s)
+                })
                 
-                # initialize tracker pointer to check correct records
-                tracker = 0
-                for index in range(tradeData.getN()):
+                # rolling window mean and std calculation
+                data["mean"] = data["Price"].rolling(self._k,center=True).mean()
+                data["std"] = data["Price"].rolling(self._k,center=True).std()
 
-                    # keep appending price till queue is full
-                    if len(self._price) < self._k-1:
-                       self._price.append(tradeData.getPrice(index))
-                       continue
-                    
-                    # append data and set parameters
-                    self._price.append(tradeData.getPrice(index))
-                    self.setParams()
+                # fixing beginning and end values
+                data.loc[N - self._k // 2  : , "mean"] = data["mean"].iloc[N-self._k//2 - 1]
+                data.loc[ : self._k // 2 , "mean" ] = data["mean"].iloc[self._k//2]
+                data.loc[N - self._k // 2 : , "std"] = data["std"].iloc[N - self._k//2 - 1]
+                data.loc[ : self._k // 2 , "std"] = data["std"].iloc[self._k//2]
 
-                    # if tracker is 0, check first k/2 records
-                    if tracker == 0:
-                       while(tracker <= (self._k // 2)):
-                           if self.checkNotOutlier(tradeData.getPrice(tracker)): 
-                              prices.append(tradeData.getPrice(tracker))
-                              shares.append(tradeData.getSize(tracker))
-                              ts.append(tradeData.getMillisFromMidn(tracker))
-                           tracker += 1 
-                       continue 
-                    
-                    # updates outliers with moving window
-                    if self.checkNotOutlier(tradeData.getPrice(tracker)):
-                        prices.append(tradeData.getPrice(tracker))
-                        shares.append(tradeData.getSize(tracker))
-                        ts.append(tradeData.getMillisFromMidn(tracker)) 
-
-                    tracker += 1
-
-                # checking end of file records
-                while(tracker < tradeData.getN()):
-                  if self.checkNotOutlier(tradeData.getPrice(tracker)): 
-                     prices.append(tradeData.getPrice(tracker))
-                     shares.append(tradeData.getSize(tracker))
-                     ts.append(tradeData.getMillisFromMidn(tracker))
-                  tracker += 1 
+                # filtering out outliers
+                data = data[ (abs(data["Price"] - data["mean"]) < 2*data["std"] + self._tau*data["mean"])]
 
                 # write updated records to clean data directory
                 BaseUtils.mkDir(MyDirectories.getTradesClDir() / date)
                 BaseUtils.writeToBinTrades(MyDirectories.getTradesClDir() / date / (ticker + '_trades.binRT'), \
-                           [tradeData.getSecsFromEpocToMidn(), len(prices)],\
-                           [ts, shares, prices])
-                
-                # clear the queue after the file is processed
-                self._price.clear()                
+                           [tradeData.getSecsFromEpocToMidn(), len(data)],\
+                           [data["time"].values.tolist(), data["Size"].values.tolist(), data["Price"].values.tolist()])         
+
 
 class TAQCleanQuotes():
     """Self suffiecent class for cleaning data
     """
-    
+
     def __init__(self, k = 5, tau = 0.0005):
        # get adjusted directory
        self._fm = FileManager( MyDirectories.getAdjDir())
        self._k = k
        self._tau = tau
-
-       #deque for tracking window of size k for bid and ask
-       self._price1 = deque([],maxlen=self._k)
-       self._price2 = deque([],maxlen=self._k)
-
-    # updates parameters on update
-    def setParams(self):
-        self._mean1 = mean(self._price1)
-        self._std1 = stdev(self._price1)
-        self._mean2 = mean(self._price2)
-        self._std2 = stdev(self._price2) 
-
-    # outlier check rule
-    def checkNotOutlier(self,price,m,std):
-        return abs(price - m) <= 2*std + self._tau*m
 
     # cleans all quote data
     def cleanAllQuotes(self, dates = None, tickers = None):
@@ -133,76 +83,52 @@ class TAQCleanQuotes():
         Dates = self._fm.getQuoteDates(dates[0], dates[1])
         for ticker in tickers:
             for date in Dates:
-                bidprices = []
-                bidsize = []
-                askprices = []
-                asksize = []
-                ts = []
 
+                # getting quotes file
                 try:
                     quoteData = self._fm.getQuotesFile( date, ticker )
                 except:
                     print(ticker, date, "FILE NOT FOUND")
                     continue
+                 
+                # setting local dataframe for the records  
+                data = pd.DataFrame({
+                'time':pd.to_numeric(quoteData._ts),
+                'askPrice':pd.to_numeric(quoteData._ap),
+                'askSize':pd.to_numeric(quoteData._as),
+                'bidPrice':pd.to_numeric(quoteData._bp),
+                'bidSize':pd.to_numeric(quoteData._bs)
+                })
 
-                tracker = 0
-                # initialize tracker pointer to check correct records
-                for index in range(quoteData.getN()):
+                # rolling window mean and std calculation
+                N = quoteData.getN()
+                data["meanbid"] = data["bidPrice"].rolling(self._k,center=True).mean()
+                data["meanask"] = data["askPrice"].rolling(self._k, center=True).mean()
+                data["stdbid"] = data["bidPrice"].rolling(self._k,center=True).std()
+                data["stdask"] = data["askPrice"].rolling(self._k,center=True).std()
 
-                    # keep appending price till queue is full
-                    if len(self._price1) < self._k-1:
-                       self._price1.append(quoteData.getBidPrice(index))
-                       self._price2.append(quoteData.getAskPrice(index))
-                       continue
+                # fixing beginning and end values
+                data.loc[N - self._k // 2 : , "meanbid"] = data["meanbid"].iloc[N-self._k//2 - 1]
+                data.loc[N - self._k // 2 : ,"meanask"] = data["meanask"].iloc[N-self._k//2 - 1]
+                data.loc[: self._k // 2 ,"meanbid"] = data["meanbid"].iloc[self._k//2]
+                data.loc[: self._k // 2 ,"meanask"] = data["meanask"].iloc[self._k//2]
+                data.loc[N - self._k // 2 : ,"stdbid"] = data["stdbid"].iloc[N-self._k//2 - 1]
+                data.loc[N - self._k // 2 : ,"stdask"] = data["stdask"].iloc[N-self._k//2 - 1]
+                data.loc[: self._k // 2 ,"stdbid"] = data["stdbid"].iloc[self._k//2]
+                data.loc[: self._k // 2 ,"stdask"] = data["stdask"].iloc[self._k//2]
 
-                    # append data and set parameters   
-                    self._price1.append(quoteData.getBidPrice(index))
-                    self._price2.append(quoteData.getAskPrice(index))
-                    self.setParams()
-
-                    # if tracker is 0, check first k/2 records and update bid ans ask
-                    if tracker == 0:
-                       while(tracker < (self._k // 2)):
-                           if self.checkNotOutlier(quoteData.getBidPrice(tracker), self._mean1,self._std1) and \
-                              self.checkNotOutlier(quoteData.getAskPrice(tracker), self._mean2,self._std2): 
-                              bidprices.append(quoteData.getBidPrice(tracker))
-                              bidsize.append(quoteData.getBidSize(tracker))
-                              ts.append(quoteData.getMillisFromMidn(tracker))
-                              askprices.append(quoteData.getAskPrice(tracker))
-                              asksize.append(quoteData.getAskSize(tracker))
-                           tracker += 1 
-                    
-                    # updates outliers with moving window
-                    if self.checkNotOutlier(quoteData.getBidPrice(tracker), self._mean1,self._std1) and \
-                        self.checkNotOutlier(quoteData.getAskPrice(tracker), self._mean2,self._std2): 
-                        bidprices.append(quoteData.getBidPrice(tracker))
-                        bidsize.append(quoteData.getBidSize(tracker))
-                        ts.append(quoteData.getMillisFromMidn(tracker))
-                        askprices.append(quoteData.getAskPrice(tracker))
-                        asksize.append(quoteData.getAskSize(tracker))
-
-                    tracker += 1
-
-                # checking end of file records
-                while(tracker < quoteData.getN()):
-                    if self.checkNotOutlier(quoteData.getBidPrice(tracker), self._mean1,self._std1) and \
-                        self.checkNotOutlier(quoteData.getAskPrice(tracker), self._mean2,self._std2): 
-                        bidprices.append(quoteData.getBidPrice(tracker))
-                        bidsize.append(quoteData.getBidSize(tracker))
-                        ts.append(quoteData.getMillisFromMidn(tracker))
-                        askprices.append(quoteData.getAskPrice(tracker))
-                        asksize.append(quoteData.getAskSize(tracker))
-                    tracker += 1 
-
+                # filtering out outliers
+                data = data[ ((abs(data["bidPrice"] - data["meanbid"]) < 2*data["stdbid"] + self._tau*data["meanbid"]) & \
+                              (abs(data["askPrice"] - data["meanask"]) < 2*data["stdask"] + self._tau*data["meanask"]))]
+ 
                 # write updated records to clean data directory
                 BaseUtils.mkDir(MyDirectories.getQuotesClDir() / date)
                 BaseUtils.writeToBinQuotes(MyDirectories.getQuotesClDir() / date / (ticker + '_quotes.binRQ'), \
-                           [quoteData.getSecsFromEpocToMidn(), len(bidprices)],\
-                           [ts, bidsize, bidprices, asksize, askprices])
+                           [quoteData.getSecsFromEpocToMidn(), len(data)],\
+                           [data["time"].values.tolist(), data["bidSize"].values.tolist(),\
+                            data["bidPrice"].values.tolist(), data["askSize"].values.tolist(), \
+                            data["askPrice"].values.tolist()])
                 
-                # clear the queue after the file is processed
-                self._price1.clear()
-                self._price2.clear()
                 
 
 
--- src/Test_TAQCleaner.py
+++ src/Test_TAQCleaner.py
@@ -7,22 +7,86 @@ from matplotlib import pyplot as plt
 
 class Test_TAQAdjust(unittest.TestCase):
 
-    def test(self):
+    # test to check correct implementatin of trades filtering
+    def test1(self):
 
+        # setting up fake trade file
+        fm2 = FileManager(MyDirectories.getCleanDir())
+        # BaseUtils.writeToBinTrades(MyDirectories.getAdjDir()/ "trades" / '20070620' / ('test' + '_trades.binRT'), \
+        #                    [2020, 5],\
+        #                    [[1,2,3,4,5],\
+        #                     [1,2,3,4,5], \
+        #                     [1,1,2,3,100]]) 
+                
+        # # cleaning trade data
+        # obj = TAQCleanTrades(k=3,tau=0.0005)
+        # obj.cleanAllTrades(tickers = ["test"], dates = ['20070620','20070621'])
+
+        # # getting cleaned data
+        # reader = fm2.getTradesFile('20070620', 'test')
+
+        # # assertions on cleaned data
+        # print(reader._p)
+        # self.assertListEqual(list(reader._p), [1,1,2,3])
+        # self.assertListEqual(list(reader._s),[1,2,3,4])
+        # self.assertListEqual(list(reader._ts), [1,2,3,4])
+        # self.assertEqual(reader.getN(),4)
+
+        # setting up fake trade file
+        BaseUtils.writeToBinTrades(MyDirectories.getAdjDir() / 'quotes' / '20070620' / ('test' + '_quotes.binRQ'), \
+                           [2020, 5],\
+                           [[1,2,3,4,5],\
+                            [1,2,3,4,5], \
+                            [1,1,2,3,100],\
+                            [1,2,3,4,5],\
+                            [1,1,2,100,5]]) 
+                
+        # cleaning quote data
+        obj = TAQCleanQuotes(k=3,tau=0.0005)
+        obj.cleanAllQuotes(tickers = ["test"], dates = ['20070620','20070621'])
+
+        # getting cleaned data
+        reader = fm2.getQuotesFile('20070620', 'test')
+
+        # assertions on cleaned data
+        print(reader._bp)
+        self.assertListEqual(list(reader._bp), [1,1,2])
+        self.assertListEqual(list(reader._bs),[1,2,3])
+        self.assertListEqual(list(reader._ap), [1,1,2])
+        self.assertListEqual(list(reader._as),[1,2,3])
+        self.assertListEqual(list(reader._ts), [1,2,3])
+        self.assertEqual(reader.getN(),3)
+
+    def test2(self):
+        # filemanger setup
         fm1 = FileManager(MyDirectories.getAdjDir())
         fm2 = FileManager(MyDirectories.getCleanDir())
-        # obj = TAQCleanTrades(k = 5, tau = 0.0005)
-        # obj.cleanAllTrades(tickers = ['MTW'], dates = ['20070620','20070621'])
-        # reader_before = fm1.getTradesFile( '20070620', 'MTW' )
-        # reader_after = fm2.getTradesFile('20070620', 'MTW')
-        # BaseUtils.plot_ba_price(reader_before, reader_after, "Trade cleaning for MTW")
+
+        # cleaning trades
+        obj = TAQCleanTrades(k = 60, tau = 0.0005)
+        obj.cleanAllTrades(tickers = ['GE'], dates = ['20070720','20070721'])
+
+        # getting before and after data
+        reader_before = fm1.getTradesFile( '20070720', 'GE' )
+        reader_after = fm2.getTradesFile('20070720', 'GE')
+
+        # saving plots to test_plot
+        BaseUtils.plot_ba_price(reader_before, reader_after, "Trade cleaning for MTW",\
+                                filename = MyDirectories.getTestPlotDir()/"test_trade_clean.png")
+        
+        # cleaning quotes data
         obj = TAQCleanQuotes(k = 60, tau = 0.0005)
         obj.cleanAllQuotes(tickers = ['GE'], dates = ['20070720','20070721'])
+
+        # getting before and after data
         reader_before = fm1.getQuotesFile( '20070720', 'GE' )
         reader_after = fm2.getQuotesFile('20070720', 'GE')
-        print(reader_before.getN(), reader_after.getN())
-        BaseUtils.plot_ba_bidp(reader_after, reader_before, "Bid cleaning for General Electric")
-        BaseUtils.plot_ba_askp(reader_after, reader_before, "Ask cleaning for General Electric")
+
+        # saving plots to test_plot
+        BaseUtils.plot_ba_bidp(reader_after, reader_before, "Bid cleaning for General Electric",\
+                               filename = MyDirectories.getTestPlotDir()/"test_bid_clean.png")
+        BaseUtils.plot_ba_askp(reader_after, reader_before, "Ask cleaning for General Electric",\
+                               filename = MyDirectories.getTestPlotDir()/"test_ask_clean.png")
         plt.show()
         
 if __name__ == "__main__":
