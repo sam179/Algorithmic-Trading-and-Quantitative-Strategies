2463435232d36a564d87db597fd1b14223a68910
Rita Liu <wliu1@macalester.edu>
2023-04-17 11:03:26 -0400

    add tests for train test split class

--- src/Test_TrainTestSplit.py
+++ src/Test_TrainTestSplit.py
@@ -2,20 +2,35 @@ import unittest
 from MyDirectories import *
 import pandas as pd
 from TrainTestSplit import TrainTestSplit
+import numpy as np
 
 class Test_TrainTestSplit(unittest.TestCase):
     tts = TrainTestSplit('normalized_returns.csv',q=0.5)
+    
     def testConstructor(self):
-        
-        self.assertEqual(len(self.tts.data.columns),509) #need to change with full data set
+        '''test for constructors'''
+        d = pd.read_csv(MyDirectories.BASE_DIR/'normalized_returns.csv',index_col=[0])
+        self.assertEqual(str(self.tts.data.columns),str(d.columns))
 
 
     def testSplit(self):
-
+        '''test that train and test sets have the same length'''
         self.tts.split()
-        self.tts.get_test_set(2)
-        self.assertFalse(self.tts.train,None)
+        self.assertEqual(len(self.tts.train),len(self.tts.test))
+        # test for corner cases
+        corner_case = TrainTestSplit('normalized_returns.csv',q=0.5)
+        corner_case.data = np.random.rand(300,200)
+        corner_case.split()
+        self.assertEqual(len(corner_case.train),0)
 
+    def testGetTrainTest(self):
+        '''test to see if they can get train test data correctly'''
+        # if fold is too large
+        with self.assertRaises(Exception):self.tts.get_train_set(70)
+        # when the fold is within range
+        self.tts.split()
+        t = self.tts.get_test_set(2)
+        self.assertEqual(len(t),self.tts.test_size)
 
 
 if __name__ == "__main__":
--- src/TrainTestSplit.py
+++ src/TrainTestSplit.py
@@ -15,30 +15,20 @@ class TrainTestSplit():
         self.train_size = int(500/self.q) #size for each train set
         self.test_size = 78
 
-        # transform data
-        # self.data['Time'] = self.data.apply(lambda x: pd.Timestamp.round(
-        #                     pd.to_datetime(
-        #                         x['MillisFromMidn'],
-        #                         unit = 'ms',
-        #                         origin=str(x.Date)),
-        #                     freq = '5T'),
-        #                 axis = 1
-        # )
-        # self.data = (self.data
-        #     .pivot_table(values = 'Returns',index = ['Time'],columns = 'Ticker')
-        #     .reset_index()
-        #     .sort_values(by = ['Time'],ignore_index=True)
-        # )
+        
         
         
     
     def get_n(self):
-        # self.n = (len(self.data)-self.train_size)//self.test_size
+        'Get the number of folds after split'
         self.n = len(self.test)
         return self.n
 
     def split(self):
         t_tol = len(self.data)
+        if t_tol<self.train_size+self.test_size:
+            print('Total length is too small to split!')
+            return 
         test_start_index = list(range(t_tol-self.test_size,self.train_size-1,-self.test_size))
         test_start_index.reverse()
         train_start_index = [i-self.train_size for i in test_start_index]
@@ -47,20 +37,6 @@ class TrainTestSplit():
         self.test = {i:list(range(test_start_index[i],test_start_index[i]+self.test_size)) 
                     for i in range(len(test_start_index))}
        
-    def get_train_index(self,fold=None):
-        # return a list of train set index at specified fold; if None, return all fold
-        # example: return [[1,2,3][4,5,6]] when fold = [1,2]
-        if fold == None:
-            return self.train
-        else:
-            return [self.tain[k] for k in fold]
-
-    def get_test_index(self,fold=None):
-        #similar to get_train_index
-        if fold == None:
-            return self.test
-        else:
-            return [self.test[k] for k in fold]
 
 
     def get_train_set(self,fold=None):
